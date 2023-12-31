43c05c52e99deeffb2853d87f9ae01eb5e81d088
Saurao Dalvi <42344640+sauraodalvi@users.noreply.github.com>
2023-12-31 17:25:43 +0530

    Update script.js
    fixed js code for long scrolling

--- script.js
+++ script.js
@@ -7,10 +7,12 @@ let hill5 = document.getElementById('hill5');
 window.addEventListener('scroll', () => {
     let value = window.scrollY;
 
-    text.style.marginTop = value * 2.5 + 'px';
-    leaf.style.top = value * -1.5 + 'px'; 
-    leaf.style.left = value * 1.5 + 'px'; 
-    hill5.style.left = value * 1.5 + 'px';
-    hill4.style.left = value * -1.5 + 'px';
-    hill1.style.top = value * 1.5 + 'px';
+    if(value < 500) {
+        text.style.marginTop = value * 2.5 + 'px';
+        leaf.style.top = value * -1.5 + 'px'; 
+        leaf.style.left = value * 1.5 + 'px'; 
+        hill5.style.left = value * 1.5 + 'px';
+        hill4.style.left = value * -1.5 + 'px';
+        hill1.style.top = value * 1.5 + 'px';
+    }
 });
